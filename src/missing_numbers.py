7d06f5bbba583de8d7c81c87a6efccab42c9fdaf
Plentiful106 <Plentiful106@users.noreply.github.com>
2025-03-13 12:56:19 +0000

    Finalize implementation of missing numbers function

--- src/missing_numbers.py
+++ src/missing_numbers.py
@@ -25,14 +25,15 @@ def find_missing_numbers(arr):
         arr = sorted(arr, reverse=True)
     
     # Find the range of numbers
+    min_num = 1
     max_num = arr[-1]
     
     # Create a set of the input array for efficient lookup
     num_set = set(arr)
     
-    # Find missing numbers from 1 to max_num
+    # Find missing numbers within the range
     missing = [
-        num for num in range(1, max_num + 1) 
+        num for num in range(min_num, max_num + 1) 
         if num not in num_set
     ]
     
