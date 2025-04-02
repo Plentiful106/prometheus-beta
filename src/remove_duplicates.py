839fcdd2f9b575bc60e4d45caddcc5528ca9c079
Plentiful106 <Plentiful106@users.noreply.github.com>
2025-04-02 16:46:56 +0000

    Implement remove_duplicates_over_two function

---
+++ src/remove_duplicates.py
@@ -0,0 +1,31 @@
+def remove_duplicates_over_two(input_string):
+    """
+    Remove characters that appear more than twice in the input string.
+    
+    Args:
+        input_string (str): The input string to process.
+    
+    Returns:
+        str: A modified string with characters appearing more than twice removed.
+    
+    Examples:
+        >>> remove_duplicates_over_two("aabbbcccc")
+        'aabbc'
+        >>> remove_duplicates_over_two("hello")
+        'hello'
+        >>> remove_duplicates_over_two("aaaaabbbbbccccc")
+        'ab'
+    """
+    if not isinstance(input_string, str):
+        raise TypeError("Input must be a string")
+    
+    # Count character occurrences
+    char_counts = {}
+    for char in input_string:
+        char_counts[char] = char_counts.get(char, 0) + 1
+    
+    # Build result string, keeping only chars that appear 1-2 times
+    result = ''.join(char for char in input_string 
+                     if char_counts[char] <= 2)
+    
+    return result
